d5239dedec89c95ff558714bcd44c250621fc615
Jherimy <mallorcajherimy42@gmail.com>
2023-08-01 16:43:09 +0800

    menu bottom at ung mga icon

--- app/src/main/java/com/example/anew/Dashboard.kt
+++ app/src/main/java/com/example/anew/Dashboard.kt
@@ -1,11 +1,11 @@
 package com.example.anew
-import android.view.MenuItem
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.view.View
 import android.widget.ImageView
-import androidx.fragment.app.Fragment
+import android.widget.PopupMenu
 import com.example.anew.databinding.ActivityDashboardBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -24,43 +24,66 @@ class Dashboard : AppCompatActivity() {
                 overridePendingTransition(R.anim.slide_in_from_left, R.anim.fade_in)
                 return@OnNavigationItemSelectedListener true
             }
-
-
-
             com.example.anew.R.id.menu_item_2 -> {
                 // Open the "Feedback" activity/form with slide-in animation
                 val intent = Intent(this, Feedback::class.java)
                 startActivity(intent)
-                overridePendingTransition(R.anim.slide_in_from_left, R.anim.fade_in)
                 return@OnNavigationItemSelectedListener true
             }
             com.example.anew.R.id.menu_item_3 -> {
                 // Open the "Feedback" activity/form with slide-in animation
                 val intent = Intent(this, Feedback::class.java)
                 startActivity(intent)
-                overridePendingTransition(R.anim.slide_in_from_left, R.anim.fade_in)
                 return@OnNavigationItemSelectedListener true
             }
-            // Add other menu items handling here if needed
         }
-        false
+        false // Return false for other cases
+    }
+
+    // Add the click listener for the ImageView
+    fun onHomeIconClick(view: View) {
+        // Create a PopupMenu
+        val popupMenu = PopupMenu(this, view)
+        // Inflate the navigation drawer menu
+        popupMenu.inflate(R.menu.navigation_drawer_menu)
+        // Set a click listener for the menu items
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_item_1 -> {
+                    // Handle menu item 1 click
+                    // You can open a new activity or perform some action here
+                    val intent = Intent(this, lolo::class.java)
+                    startActivity(intent)
+                    overridePendingTransition(R.anim.slide_in_from_left, R.anim.fade_in)
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.menu_item_2 -> {
+                    // Handle menu item 2 click
+                    // You can open a new activity or perform some action here
+                    val intent = Intent(this, Feedback::class.java)
+                    startActivity(intent)
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.menu_item_3 -> {
+                    // Handle menu item 3 click
+                    // You can open a new activity or perform some action here
+                    val intent = Intent(this, Feedback::class.java)
+                    startActivity(intent)
+                    return@setOnMenuItemClickListener true
+                }
+                else -> false
+            }
+        }
+        // Show the PopupMenu
+        popupMenu.show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        homeIconImageView = findViewById(R.id.homeic2)
-        homeIconImageView.setOnClickListener {
-            val intent = Intent(this, lolo::class.java)
-            startActivity(intent)
-            overridePendingTransition(R.anim.slide_in_from_left, R.anim.fade_in)
-        }
-
-        // Set up the bottom navigation view listener using an instance of OnNavigationItemSelectedListener
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            bottomNavItemSelectedListener.onNavigationItemSelected(item)
-        }
+        // Set the bottomNavItemSelectedListener to the BottomNavigationView
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavItemSelectedListener)
     }
 }
