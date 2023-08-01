c418015e77da9e7cd9add4953fea1c075f5a4da1
Kurt Bryant C. Arpilleda <BCArpilleda@gmail.com>
2023-08-01 14:12:34 +0800

    Manifest and EmergencyContact

---
+++ app/src/main/java/com/example/anew/EmergencyContact.kt
@@ -0,0 +1,11 @@
+package com.example.anew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmergencyContact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emergency_contact)
+    }
+}
