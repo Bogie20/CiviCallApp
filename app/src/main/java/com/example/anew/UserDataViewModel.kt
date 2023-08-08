06a312d6d6c29d865ccdccdae53b5e0c6c26ebeb
Jherimy <mallorcajherimy42@gmail.com>
2023-08-08 09:10:55 +0800

    name change in lolo

---
+++ app/src/main/java/com/example/anew/UserDataViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.anew
+
+import androidx.lifecycle.ViewModel
+
+class UserDataViewModel: ViewModel() {
+    var uid: String? = null
+    var fname: String? = null
+    var lname: String? = null
+    var email: String? = null
+    }
