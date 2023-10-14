f11c355a76e95b468c6591e3011d65b552c91d09
Jherimy <mallorcajherimy42@gmail.com>
2023-10-14 12:40:38 +0800

    civic responsibility  isa nalang kulang na title

---
+++ app/src/main/java/com/example/civicall/CivicRightsAndResponsibility/DataMain.kt
@@ -0,0 +1,3 @@
+package com.example.civicall.CivicRightsAndResponsibility
+
+data class DataMain(val title: String)
---
+++ app/src/main/java/com/example/civicall/CivicRightsAndResponsibility/GlobalCitizenshipandCivicResponsibilityInfo.kt
@@ -0,0 +1,12 @@
+package com.example.civicall.CivicRightsAndResponsibility
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.civicall.R
+
+class GlobalCitizenshipandCivicResponsibilityInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_global_citizenshipand_civic_responsibility_info)
+    }
+}
