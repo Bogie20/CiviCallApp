49834c71286a0db39358d462dfb20e9cd9ed19bb
Jherimy <mallorcajherimy42@gmail.com>
2023-10-31 23:00:35 +0800

    background konti nalang

--- app/src/main/java/com/example/civicall/Accountverification.kt
+++ app/src/main/java/com/example/civicall/Accountverification.kt
@@ -9,12 +9,16 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
 import android.provider.OpenableColumns
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class Accountverification : AppCompatActivity() {
     private lateinit var BackClick: ImageView
@@ -25,22 +29,38 @@ class Accountverification : AppCompatActivity() {
     private var isFileUploaded = false
     private lateinit var sharedPreferences: SharedPreferences
     private val FILE_NAME_KEY = "file_name_key"
+    private val storage = FirebaseStorage.getInstance()
+    private val storageRef = storage.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accountverification)
-
         BackClick = findViewById(R.id.back100)
         BackClick.setOnClickListener {
             val intent = Intent(this, Dashboard::class.java)
             startActivity(intent)
         }
 
-        uploadButton = findViewById(R.id.button223)
+        uploadButton = findViewById(R.id.uploadbutton)
         fileNameTextView = findViewById(R.id.selectedFileNameTextView)
 
         // Get the SharedPreferences instance
         sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE)
+
+        val buttonRemove = findViewById<Button>(R.id.buttonremove)
+        buttonRemove.setOnClickListener {
+            // Reset the verification status
+            isFileUploaded = false
+
+            // Clear the file name from SharedPreferences
+            val editor = sharedPreferences.edit()
+            editor.remove(FILE_NAME_KEY)
+            editor.apply()
+
+            // Update the UI to reflect the changes
+            fileNameTextView.text = "Not Yet Verified"
+            uploadButton.isEnabled = true
+        }
 
         // Retrieve the file name from SharedPreferences if it exists
         val savedFileName = sharedPreferences.getString(FILE_NAME_KEY, null)
@@ -86,7 +106,7 @@ class Accountverification : AppCompatActivity() {
 
     private fun openDocumentPicker() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "*/*" // Allow all file types
+        intent.type = "application/pdf" // Specify the MIME type for PDF files
         startActivityForResult(intent, REQUEST_CODE_DOCUMENT_PICKER)
     }
 
@@ -94,36 +114,48 @@ class Accountverification : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE_DOCUMENT_PICKER && resultCode == RESULT_OK) {
             data?.data?.let { uri ->
-                // Check if the selected file has the desired MIME type (PDF or Word document)
+                // Check if the selected file has the desired MIME type (PDF)
                 if (isFileAllowed(uri)) {
                     // Get the file name from the URI
                     val fileName = getFileName(uri)
 
-                    // Save the file name in SharedPreferences
-                    val editor = sharedPreferences.edit()
-                    editor.putString(FILE_NAME_KEY, fileName)
-                    editor.apply()
+                    // Upload the file to Firebase Storage
+                    val storageRef = FirebaseStorage.getInstance().reference
+                    val fileReference = storageRef.child("verification File/$fileName")
 
-                    // Show the confirmation dialog before showing the popup dialog
-                    showConfirmationDialog(fileName)
+                    fileReference.putFile(uri)
+                        .addOnSuccessListener { _ ->
+                            // File successfully uploaded
+                            Toast.makeText(this, "File uploaded to Firebase Storage", Toast.LENGTH_SHORT).show()
+
+                            // Save the file name in SharedPreferences
+                            val editor = sharedPreferences.edit()
+                            editor.putString(FILE_NAME_KEY, fileName)
+                            editor.apply()
+
+                            // Show the confirmation dialog before showing the popup dialog
+                            showConfirmationDialog(fileName)
+                        }
+                        .addOnFailureListener { e ->
+                            // Handle the error when file upload fails
+                            Log.e("Firebase Storage", "File upload failed: $e")
+                            Toast.makeText(this, "File upload failed", Toast.LENGTH_SHORT).show()
+                        }
                 } else {
                     // Display a message or handle the case when an invalid file type is selected.
                     // For example, show a toast or dialog informing the user about the allowed file types.
-                    Toast.makeText(this, "Only PDF and Word documents are allowed.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Only PDF documents are allowed.", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
 
-    // Helper function to check if the selected file has the desired MIME type (PDF or Word document)
     private fun isFileAllowed(uri: Uri): Boolean {
         val contentResolver: ContentResolver = contentResolver
         val mime = contentResolver.getType(uri)
-        return mime?.startsWith("application/pdf") == true ||
-                mime?.startsWith("application/vnd.openxmlformats-officedocument.wordprocessingml.document") == true
+        return mime?.startsWith("application/pdf") == true
     }
 
-    // Helper function to get the file name from the URI
     private fun getFileName(uri: Uri): String {
         val contentResolver: ContentResolver = contentResolver
         val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
@@ -144,5 +176,3 @@ class Accountverification : AppCompatActivity() {
         fragment.show(fragmentTransaction, "popup_dialog")
     }
 }
-
-
