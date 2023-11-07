4bbd344c2ac60015eef2ac12d410e80d70bea379
Jherimy <mallorcajherimy42@gmail.com>
2023-11-07 20:03:14 +0800

    tapos na ui

--- app/src/main/java/com/example/civicall/AccountVerification/GovernmentId.kt
+++ app/src/main/java/com/example/civicall/AccountVerification/GovernmentId.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.civicall.R
 
-class ParentConsent : AppCompatActivity() {
+class GovernmentId : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_parent_consent)
+        setContentView(R.layout.activity_government_id)
     }
 }
