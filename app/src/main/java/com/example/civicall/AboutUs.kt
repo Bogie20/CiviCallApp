4ae4556ff037e76bd1c64570d4afd8798691edaa
kbarpilleda <BCArpilleda@gmail.com>
2023-11-09 21:40:02 +0800

    detailpost goods

--- app/src/main/java/com/example/civicall/AboutUs.kt
+++ app/src/main/java/com/example/civicall/AboutUs.kt
@@ -8,19 +8,17 @@ import com.example.civicall.databinding.ActivityAboutUsBinding
 
 class AboutUs : AppCompatActivity() {
     private lateinit var networkUtils: NetworkUtils
-    private lateinit var binding: ActivityAboutUsBinding
-    private lateinit var BackClick: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
-        BackClick = findViewById(R.id.back100)
         networkUtils = NetworkUtils(this)
         networkUtils.initialize()
 
-        BackClick.setOnClickListener {
-            // Open the "Menu" activity/form
-            val intent = Intent(this, Dashboard::class.java)
-            startActivity(intent)
+        val backButton: ImageView = findViewById(R.id.backbtn)
+        backButton.setOnClickListener {
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            overridePendingTransition(R.anim.animate_fade_enter, R.anim.animate_fade_exit)
+            onBackPressed()
         }
     }
     override fun onDestroy() {
